6af00187010741eb2d24e95ed31c80bdc8ceec47
Alex Kozak <56194044+Alex-byte256@users.noreply.github.com>
2024-11-05 23:38:52 +0200

    [inProgress]calculatorVol2

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,45 +1,114 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {KeyValuePipe, NgForOf, NgIf} from "@angular/common";
+
+interface CalcGroup{
+  first: CalcVar,
+  second: CalcVar,
+  operation:CalcOperations
+}
+
+interface CalcVar {
+  value: number,
+  modificator: CalcModifiers
+}
+
+enum CalcOperations{
+  plus = "+",
+  minus = "-",
+  multiply = "*",
+  divide = "/"
+}
+
+enum CalcModifiers{
+  none = "none",
+  sin = "sin",
+  cos = "cos",
+  square = "square"
+}
 
 @Component({
   selector: 'app-my-calculator',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    KeyValuePipe,
+    NgIf
   ],
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.scss'
 })
 export class MyCalculatorComponent {
- public first :number = 1;
- public second :number = 1;
+  public calcOperations = CalcOperations;
+  public calcModifiers = CalcModifiers;
 
- public operation :string = "+";
+  public CalcGroups: CalcGroup[]= [
+    {
+      first:{
+        value:5,
+        modificator:CalcModifiers.none
+      },
+      second:{
+        value:5,
+        modificator:CalcModifiers.none
+      },
+      operation:CalcOperations.plus,
+    }
+  ]
 
- public operations :string[] = ["+","-","*","/"];
+  public history :string[] = []
+
+  public operationsBetweenGroups :CalcOperations[] = []
+
+
 
  // public result :number | undefined = undefined;
  public result?: number;
 
- public Calc(){
-      switch (this.operation){
-        case "+":
-          this.result = this.first + this.second;
-          break
-        case "-":
-          this.result = this.first - this.second;
-          break
-        case "*":
-          this.result = this.first * this.second;
-          break
-        case "/":
-          this.result = this.first / this.second;
-          break
-        default:
-          this.result = undefined;
-          break
+  public addGroup() :void{
+    this.CalcGroups.push(
+      {
+        first:{
+          value:0,
+          modificator:CalcModifiers.none
+        },
+        second:{
+          value:0,
+          modificator:CalcModifiers.none
+        },
+        operation:CalcOperations.plus,
       }
- }
+    )
+
+    this.operationsBetweenGroups.push(
+      CalcOperations.plus
+    )
+  }
+
+
+  public removeGroup(index : number)  {
+    this.CalcGroups.splice(index, 1);
+  }
+
+ // public Calc(){
+ //      switch (this.operation){
+ //        case "+":
+ //          this.result = this.first + this.second;
+ //          break
+ //        case "-":
+ //          this.result = this.first - this.second;
+ //          break
+ //        case "*":
+ //          this.result = this.first * this.second;
+ //          break
+ //        case "/":
+ //          this.result = this.first / this.second;
+ //          break
+ //        default:
+ //          this.result = undefined;
+ //          break
+ //      }
+ // }
+  protected readonly CalcOperations = CalcOperations;
 }
